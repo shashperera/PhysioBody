49b7aa208a2935330fbe7db808995b7415a7908b
shashperera <sanduni.p@sliit.lk>
2023-07-25 21:16:48 +0530

    Update pose comparison

--- pose_compare.py
+++ pose_compare.py
@@ -1,12 +1,3 @@
-"""Motivation : https://arxiv.org/pdf/1906.12171.pdf
-
-@article{schneider2019gesture,
-  title={Gesture Recognition in RGB Videos Using Human Body Keypoints and Dynamic Time Warping},
-  author={Schneider, Pascal and Memmesheimer, Raphael and Kramer, Ivanna and Paulus, Dietrich},
-  journal={arXiv preprint arXiv:1906.12171},
-  year={2019}
-}
-"""
 import csv
 import math
 import sys
@@ -17,32 +8,14 @@ from typing import List
 import fastdtw
 import numpy as np
 
-from scipy.ndimage.filters import gaussian_filter
+from scipy.ndimage import gaussian_filter
 from scipy.signal import medfilt
 from scipy.spatial.distance import euclidean
-
+import matplotlib.pyplot as plt
 from main import CocoPart
 
 
 def load_csv(csv_fp: str) -> List:
-    """Load keypoint coordinates stored in a CSV file.
-
-    Columns are in order frame_no, nose.(x|y|p), (l|r)eye.(x|y|p), (l|r)ear.(x|y|p), (l|r)shoulder.(x|y|p),
-    (l|r)elbow.(x|y|p), (l|r)wrist.(x|y|p), (l|r)hip.(x|y|p), (l|r)knee.(x|y|p), (l|r)ankle.(x|y|p)
-
-    l - Left side of the identified joint
-    r - Right side of the identified joint
-    x - X coordinate of the identified joint
-    y - Y coordinate of the identified joint
-    p - Probability of the identified joint
-
-    Coordinate list for a frame = [ [x, y], [x, y], [x, y], ... ]
-    *coordinates in order specified in the CSV header
-
-    Returns a list of coordinates for each frame = [ [...], [...], ... ]
-
-    :param csv_fp: Path to the CSV file
-    """
     pose_coordinates = []
 
     with open(csv_fp, 'r') as csv_file:
@@ -50,88 +23,68 @@ def load_csv(csv_fp: str) -> List:
         next(reader)
 
         for row in reader:
-            # TODO: Figure out a way to handle missing joints
-            coordinates = []  # List to store XY coordinates of every joint
+            coordinates = []
+            for index in range(1, len(row), 3):
+                x_str, y_str = row[index], row[index + 1]
+                if x_str and y_str:
+                    coordinates.append([float(x_str), float(y_str)])
+                else:
+                    coordinates.append([0.0, 0.0])
 
-            # Fill the coordinate list for a single frame
-            for index in range(1, len(row), 3):  # Starting from 1 to skip frame column, 3 because joint.x|y|prob
-                coordinates.append([float(row[index]), float(row[index + 1])])
-
-            # Fill the final list with coordinate lists of all the frames
             pose_coordinates.append(coordinates)
 
     return pose_coordinates
 
-#Translation and scaling
+
 def normalize(all_coordinates: List) -> List:
-    """The normalization is a simple coordinate transformation done in two steps:
+    norm_coords = []
 
-    1. Translation: All the key points are translated such that the nose key point becomes the origin of the coordinate
-        system. This is achieved by subtracting the nose key points coordinates from all other key points.
-
-    2. Scaling: The key points are scaled such that the distance between the left shoulder and right shoulder key point
-        becomes 1. This is done by dividing all key points coordinates by the distance between the left and right
-        shoulder key point.
-    """
-    norm_coords = []  # Hold the normalised coordinates for every frame
-
-    # Iterating over every frame
     for coordinates in all_coordinates:
-        # Step 1: Translate
-        coordinates = [
-            [coordinate[0] - coordinates[CocoPart.Nose.value][0], coordinate[1] - coordinates[CocoPart.Nose.value][1]]
-            for coordinate in coordinates
-        ]
+        if len(coordinates) < 2:
+            continue  # Skip frames with insufficient keypoints
 
-        # Step 2: Scale
-        dist = math.hypot(coordinates[CocoPart.LShoulder.value][0] - coordinates[CocoPart.RShoulder.value][0],
-                          coordinates[CocoPart.LShoulder.value][1] - coordinates[CocoPart.RShoulder.value][1])
-        coordinates = [[coordinate[0] / dist, coordinate[1] / dist] for coordinate in coordinates]
+        frame_no = coordinates[0]  # Extract the frame number (assuming it's the first element)
 
-        norm_coords.append(coordinates)
+        # Extract the keypoints (excluding the frame number)
+        keypoints = coordinates[1:]
+
+        # Step 1: Translation (subtract the first keypoint from all keypoints)
+        origin = keypoints[0]
+        translated_coords = [[coord[0] - origin[0], coord[1] - origin[1]] for coord in keypoints]
+
+        # Step 2: Scaling (normalize by the distance between two specified keypoints)
+        reference_dist = math.hypot(translated_coords[3][0] - translated_coords[4][0],
+                                    translated_coords[3][1] - translated_coords[4][1])
+        scaled_coords = [[coord[0] / reference_dist, coord[1] / reference_dist] for coord in translated_coords]
+
+        # Add the normalized keypoints back to the frame
+        normalized_frame = [frame_no] + scaled_coords
+
+        norm_coords.append(normalized_frame)
 
     return norm_coords
 
-#Filter indices 
+
 def dimension_selection(frames: List) -> List:
-    """Remove indices that don't vary a lot during the pose.
-
-    Key points that do not move significantly in the sequence will cause the signals of the respective coordinates to be
-    roughly constant with only little variance. All signals whose variance is below a threshold will be filtered out and
-    are assumed to be uninformative.
-
-    :returns: A set of indices of dimensions that should be kept
-    """
-
-    def keep_sequence(seq: List) -> bool:   
-        """Check whether the points in the sequence vary a lot or not."""
-        # Use a median filter for smoothing
+    def keep_sequence(seq: List) -> bool:
         seq = medfilt(seq, kernel_size=3)
-
-        # Filter the dimension based on variance
         return np.var(seq) > 0.10
 
-    # Reorder the coordinates such that they are per joint and not per frame
-    frames = [list(chain(*frame)) for frame in frames]  # Flatten the nested lists inside
-    sequences = list(map(list, zip(*frames)))  # Transpose the list
+    frames = [list(chain(*frame)) for frame in frames]
+    sequences = list(map(list, zip(*frames)))
 
-    # Drop low variance columns
     dimensions = [i for i, sequence in enumerate(sequences) if keep_sequence(sequence)]
 
     return dimensions
 
-# Calculate how similar the two pose sequences are."""
+
+def process_signal(signal: List) -> List:
+    signal = gaussian_filter(signal, sigma=1)
+    mean = np.mean(signal)
+    return [x - mean for x in signal]
+
+
 def calculate_score(seq1: List, seq2: List, dimensions: List) -> float:
-
-    def process_signal(signal: List) -> List:
-        """Final processing before dynamic time warping."""
-        # Apply Gaussian filter for further processing
-        signal = gaussian_filter(signal, sigma=1)
-
-        # Make the sequence/signal zero-mean by subtracting the mean from it
-        mean = np.mean(signal)
-        return [x - mean for x in signal]
-
     distance = 0.0
 
     for dim in dimensions:
@@ -141,44 +94,87 @@ def calculate_score(seq1: List, seq2: List, dimensions: List) -> float:
         temp_distance, _ = fastdtw.fastdtw(sig1, sig2, radius=30, dist=euclidean)
         distance += temp_distance
 
-    # Normalise DTW score
     distance /= len(dimensions)
 
     return distance
 
 
+def plot_signals(seq1: List, seq2: List, dimensions: List):
+    for dim in dimensions:
+        plt.figure(figsize=(10, 5))
+        
+        # Original signals
+        plt.subplot(2, 1, 1)
+        plt.plot(seq1[dim], label='Sequence 1')
+        plt.plot(seq2[dim], label='Sequence 2')
+        plt.title(f'Dimension {dim + 1} - Original Signals')
+        plt.legend()
+
+        # Processed signals
+        processed_seq1 = process_signal(signal=seq1[dim])
+        processed_seq2 = process_signal(signal=seq2[dim])
+        plt.subplot(2, 1, 2)
+        plt.plot(processed_seq1, label='Processed Sequence 1')
+        plt.plot(processed_seq2, label='Processed Sequence 2')
+        plt.title(f'Dimension {dim + 1} - Processed Signals')
+        plt.legend()
+
+        plt.tight_layout()
+        plt.show()
+
+        
+
 def main():
-    # CSV file paths
-    pose_csv1 = sys.argv[1]
-    pose_csv2 = sys.argv[2]
+    if len(sys.argv) < 3:
+        print("Usage: python pose_compare.py keypoint_csv1 keypoint_csv2")
+        return
 
-    # Load the CSVs
-    pose1 = load_csv(csv_fp=pose_csv1)
-    pose2 = load_csv(csv_fp=pose_csv2)
+    keypoint_csv1 = sys.argv[1]
+    keypoint_csv2 = sys.argv[2]
 
-    # Normalization
-    pose1 = normalize(pose1)
-    pose2 = normalize(pose2)
+    keypoints1 = load_csv(csv_fp=keypoint_csv1)
+    keypoints2 = load_csv(csv_fp=keypoint_csv2)
 
-    # Dimension Selection
-    pose1_dimensions = dimension_selection(pose1.copy())
-    pose2_dimensions = dimension_selection(pose2.copy())
+    keypoints1 = normalize(keypoints1)
+    keypoints2 = normalize(keypoints2)
 
-    dimensions = sorted(set(pose1_dimensions + pose2_dimensions))  # Take a union to get final list of dimensions
+    keypoints1_dimensions = dimension_selection(keypoints1.copy())
+    keypoints2_dimensions = dimension_selection(keypoints2.copy())
 
-    score = calculate_score(pose1, pose2, dimensions)
+    dimensions = sorted(set(keypoints1_dimensions + keypoints2_dimensions))
+
+    score = calculate_score(keypoints1, keypoints2, dimensions)
     print(f'Score = {score:.6f}')
 
+    # Plot the signals for comparison
+    # plot_signals(keypoints1, keypoints2, dimensions)
+
+   # Calculate the difference in keypoints between the two CSV files
+    keypoints_diff = []
+    for keypoint1, keypoint2 in zip(keypoints1, keypoints2):
+        keypoint_diff = [abs(coord1[0] - coord2[0]) + abs(coord1[1] - coord2[1]) for coord1, coord2 in zip(keypoint1[1:], keypoint2[1:])]
+        keypoints_diff.append(keypoint_diff)
+
+    # Get the keypoints difference for the 20th frame
+    frame_num = 20
+    keypoints_diff_20th_frame = keypoints_diff[frame_num - 1]
+
+    # Create a bar graph for the difference in keypoints for the 20th frame
+    num_keypoints = len(keypoints_diff_20th_frame)
+    width = 0.4  # Width of each bar
+
+    plt.figure(figsize=(10, 6))
+    x = np.arange(num_keypoints)
+
+    plt.bar(x, keypoints_diff_20th_frame, width=width)
+    plt.xlabel('Keypoint')
+    plt.ylabel('Keypoints Difference')
+    plt.title(f'Difference in Keypoints - Frame {frame_num}')
+    plt.xticks(x, [f'Keypoint {i+1}' for i in range(num_keypoints)])
+    # Display the accuracy score as text above the bar graph
+    plt.text(0.5, 1.05, f'Accuracy Score = {score:.6f}', transform=plt.gca().transAxes, ha='center')
+    
+    plt.show()
 
 if __name__ == '__main__':
     main()
-
-# import numpy as np
-# from scipy.spatial.distance import euclidean
-
-# from fastdtw import fastdtw
-
-# x = np.array([[1,1], [2,2], [3,3], [4,4], [5,5]])
-# y = np.array([[2,2], [3,3], [4,4]])
-# distance, path = fastdtw(x, y, dist=euclidean)
-# print(distance)
